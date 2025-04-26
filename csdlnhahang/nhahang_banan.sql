23cf4f9719f9aa7f095a0099cb17effdb9595097
Hieu <hieuld200103@gmail.com>
2025-04-27 01:59:37 +0700

    Cập nhật chức năng A

---
+++ csdlnhahang/nhahang_banan.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: nhahang
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `banan`
+--
+
+DROP TABLE IF EXISTS `banan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `banan` (
+  `ID_BanAn` int unsigned NOT NULL AUTO_INCREMENT,
+  `Soghe` int NOT NULL,
+  `TrangThai` enum('TRONG','DA_DAT','DANG_SU_DUNG') DEFAULT 'TRONG',
+  `ID_ChiNhanh` int unsigned NOT NULL,
+  PRIMARY KEY (`ID_BanAn`),
+  KEY `fk_banan_chinhanh` (`ID_ChiNhanh`),
+  CONSTRAINT `fk_banan_chinhanh` FOREIGN KEY (`ID_ChiNhanh`) REFERENCES `chinhanh` (`ID_ChiNhanh`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `banan`
+--
+
+LOCK TABLES `banan` WRITE;
+/*!40000 ALTER TABLE `banan` DISABLE KEYS */;
+INSERT INTO `banan` VALUES (1,4,'TRONG',1),(2,4,'TRONG',1),(3,4,'TRONG',1),(4,4,'TRONG',1),(5,2,'TRONG',1),(6,2,'TRONG',1),(7,2,'TRONG',1),(8,2,'TRONG',1),(9,6,'TRONG',1),(10,6,'TRONG',1),(11,8,'TRONG',1),(12,8,'TRONG',1),(13,4,'TRONG',2),(14,4,'TRONG',2),(15,4,'TRONG',2),(16,4,'TRONG',2),(17,4,'TRONG',2),(18,4,'TRONG',2),(20,4,'TRONG',1),(21,2,'TRONG',1),(22,2,'TRONG',1),(23,2,'TRONG',1),(24,2,'TRONG',1);
+/*!40000 ALTER TABLE `banan` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-25  1:01:21
---
+++ csdlnhahang/nhahang_user.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: nhahang
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `ID_User` int unsigned NOT NULL AUTO_INCREMENT,
+  `TenUser` varchar(50) NOT NULL,
+  `SDT` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `MatKhau` varchar(255) NOT NULL,
+  `Role` enum('SLIVER','','GOLD','DIAMOND') DEFAULT 'SLIVER',
+  PRIMARY KEY (`ID_User`),
+  UNIQUE KEY `SDT` (`SDT`),
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (12,'Lưu Đức Hiếu','0976981804','hieuld@gmail.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','DIAMOND'),(13,'Lưu Thu An','0976180420','an@gmail.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','SLIVER'),(14,'Hoàng','0976984315','hung@gmail.com','6c658ee83fb7e812482494f3e416a876f63f418a0b8a1f5e76d47ee4177035cb','SLIVER'),(15,'Hiền','0631356974','hien@gmail.com','e54fc6b51915e222ba6196747a19ebb8dfa651fd2b46a385a0ded647fbfefda0','GOLD'),(16,'Huynh','0986572917','huynh@gmail.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','GOLD'),(17,'Hiếu dz','0123456789','hieudz@gmail.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','SLIVER'),(18,'Nguyễn Hữu Cầm','0976354824','camnh@gmail.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','SLIVER');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-25  1:01:22
